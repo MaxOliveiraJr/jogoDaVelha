dbc885adc98ae1e92d6e7008edf788ee03f1215e
Max Oliveira <max-oliveira@outlook.com.br>
2022-05-08 17:01:00 -0300

    finalizado regra de vencedor

--- interface.js
+++ interface.js
@@ -1,27 +1,35 @@
 
-document. addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     let squares = document.querySelectorAll('.square');
 
-    squares.forEach((squere)=>{
+    squares.forEach((squere) => {
         squere.addEventListener('click', handleClick);
     })
 })
 
-function handleClick(event){
+function handleClick(event) {
     let square = event.target;
     let position = square.id;
-    handleMove(position);
+    if (handleMove(position)) {
+
+        setTimeout(function () {
+            alert('O jogo acabou');
+        },500)
+
+    };
+
+
     updateSqueres();
 }
 
-function updateSqueres(){
+function updateSqueres() {
     let squares = document.querySelectorAll('.square');
 
-    squares.forEach((square)=>{
+    squares.forEach((square) => {
         let position = square.id;
         let symbol = board[position];
 
-        if(symbol != ''){
+        if (symbol != '') {
             square.innerHTML = `<div class='${symbol}'></div>`;
         }
     });
